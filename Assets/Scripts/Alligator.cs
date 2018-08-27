3edfd8fe05240b3f39075bafba084331382f0427
Berke Kagan Nohut <e2036135@ceng.metu.edu.tr>
2018-08-27 16:21:12 +0300

    Alligator movement with navmesh.

---
+++ Assets/Scripts/Alligator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Alligator : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    [SerializeField] private float _randomness;
+    [SerializeField] private float _radius;
+
+    private void Awake()
+    {
+        _agent = gameObject.GetComponent<NavMeshAgent>();
+        StartCoroutine(Live(1f));
+
+    }
+    private IEnumerator Live(float time)
+    {
+        Debug.Log("Waiting for : " + time + " seconds.");
+        yield return new WaitForSeconds(time);
+
+        Vector3 randomDirection = Random.insideUnitSphere * _radius;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, _radius, 1);
+        Vector3 finalPosition = hit.position;
+        _agent.destination = finalPosition;
+
+        StartCoroutine(Live(Random.Range(1f, _randomness < 1f ? 1f : _randomness)));
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+
+}
---
+++ Assets/Scripts/cube.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 3);
+    }
+}
