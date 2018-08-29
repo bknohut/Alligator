9d550e8c1b3f1c06b8022735d697db77917cc88d
Berke Kagan Nohut <e2036135@ceng.metu.edu.tr>
2018-08-29 14:37:32 +0300

    Animations added.

--- Assets/Scripts/Alligator.cs
+++ Assets/Scripts/Alligator.cs
@@ -2,14 +2,27 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum AlligatorState
+{
+    idle = 0,
+    moving = 1
+}
 
 public class Alligator : MonoBehaviour
-{
+{   
+ #region Variables
+
     private NavMeshAgent _agent;
+    private Animator _animator;
+
+    [SerializeField] private Avatar _idleAvatar;
+    [SerializeField] private Avatar _walkAvatar;
 
     private float _minAcceleration = 1f;
     private float _minSpeed = 1f;
     private float _minIdleTime = 1f;
+
+    private AlligatorState _currentState;
 
     [SerializeField] private float _randomness;
     [SerializeField] private float _radius;
@@ -18,6 +31,7 @@ public class Alligator : MonoBehaviour
     [SerializeField] private float _maxAcceleration;
     [SerializeField] private float _maxIdleTime;
 
+    private Coroutine _coroutine;
 
     public float Randomness
     {
@@ -25,32 +39,89 @@ public class Alligator : MonoBehaviour
         set { _randomness = value; }
     }
 
+ #endregion
+ #region Methods
+
     private void Awake()
     {
-        _agent = gameObject.GetComponent<NavMeshAgent>();
-        StartCoroutine(Live(_minIdleTime));
-    }
-    private IEnumerator Live(float time)
-    {
-        Debug.Log("Waiting for : " + time + " seconds.");
-        yield return new WaitForSeconds(time);
-        
-        Vector3 randomDirection = Random.insideUnitSphere * _radius + transform.position;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, _radius, 1);
-        Vector3 finalPosition = hit.position;
+        _agent    = gameObject.GetComponent<NavMeshAgent>();
+        _animator = gameObject.GetComponent<Animator>();
+        _currentState = AlligatorState.idle;
 
-        SetMovementProperties(finalPosition);
-
-        float interpolation = GetNoise();
-        float idleTime = Mathf.Lerp(_minIdleTime, _maxIdleTime, interpolation);
-        StartCoroutine(Live(idleTime));
+        StartCoroutine(Live());
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, _radius);
     }
+
+    private IEnumerator Live()
+    {
+        if (_coroutine != null) StopCoroutine(_coroutine);
+        _currentState = (AlligatorState)Random.Range(0, 2);
+
+        if (_currentState == AlligatorState.idle)
+        {
+            _coroutine = StartCoroutine(StayIdle());
+        }
+        else if (_currentState == AlligatorState.moving)
+        {
+            _coroutine = StartCoroutine(Move(true));
+        }
+        yield return null;
+    }
+    private IEnumerator StayIdle()
+    {
+        _animator.avatar = _idleAvatar;
+        _animator.SetBool("IsIdle", true);
+
+        float interpolation = GetNoise();
+        float idleTime = Mathf.Lerp(_minIdleTime, _maxIdleTime, interpolation);
+        Debug.Log("I AM IDLE FOR " + idleTime + " BITCH");
+        yield return new WaitForSeconds(idleTime);
+
+        StartCoroutine(Live());
+    }
+    // must be rechecked
+    private IEnumerator Move(bool isMovementNew = false)
+    {
+        if (isMovementNew)
+        {
+            Debug.Log("I AM MOOving");
+
+            Vector3 randomDirection = Random.insideUnitSphere * _radius + transform.position;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, _radius, 1);
+            Vector3 finalPosition = hit.position;
+
+            _animator.avatar = _walkAvatar;
+            _animator.SetBool("IsIdle", false);
+            SetMovementProperties(finalPosition);
+            yield return new WaitForSeconds(Random.Range(0.2f, 0.3f));
+            _coroutine = StartCoroutine(Move());
+        }
+
+        else if (!_agent.pathPending)
+        {
+            if (_agent.remainingDistance <= _agent.stoppingDistance)
+            {
+                StartCoroutine(Live());
+            }
+            else
+            {
+                yield return new WaitForSeconds(Random.Range(0.2f, 0.3f));
+                _coroutine = StartCoroutine(Move());
+            }
+        }
+        else
+        {
+            yield return new WaitForSeconds(Random.Range(0.2f, 0.3f));
+            _coroutine = StartCoroutine(Move());
+        }
+
+    }
+
     private void SetMovementProperties(Vector3 finalPosition)
     {
         float interpolation = GetNoise();
@@ -58,7 +129,6 @@ public class Alligator : MonoBehaviour
         _agent.acceleration = Mathf.Lerp(_minAcceleration, _maxAcceleration, interpolation);
         _agent.destination = finalPosition;
     }
-
     private float GetNoise()
     {
         // get noise from random coordinates of the noise field
@@ -71,4 +141,6 @@ public class Alligator : MonoBehaviour
 
         return noise;
     }
+
+#endregion
 }
